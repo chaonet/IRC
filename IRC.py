74a043971b013c5ad5f2633528046247ac22afc6
chao xu <chao@chaodeMBP.lan>
2015-09-10 09:18:00 +0800

    python ok

--- IRC.py
+++ IRC.py
@@ -25,13 +25,20 @@ class CommandHandler:
             except IndexError:
                 line = None
             meth = getattr(self, 'do_'+cmd, None)  # self ?？，和 房间绑定的？？
+            print meth
+            # <bound method Hall.do_login of <__main__.Hall instance at 0x1011105a8>>
+            # <bound method Hall.do_python of <__main__.Hall instance at 0x1011105a8>>
             try:
                 meth(session, line)
             except TypeError:
                 self.unknow(session, cmd)
-                # print 22
+                print 22
         else:
-            self.unknow(session, line)
+            meth = getattr(self, 'do_broadcast', line)
+            try:
+                meth(session, line)
+            except TypeError:
+                self.unknow(session, line)
 
     def unknow(self, session, cmd):
         session.send('Unknow command: %s\r\n' % cmd)
@@ -51,8 +58,11 @@ class Room(CommandHandler):
         self.sessions.remove(session)
 
     def broadcast(self, session, line):
-        for session in self.sessions:
-            session.push(line + '\r\n')
+        print session,' session'
+        print self.sessions,' self.sessions'
+        for i in self.sessions:
+            if i != session:
+                i.push(line + '\r\n')
 
     def do_logout(self,session,line):
         raise EndSession
@@ -69,13 +79,17 @@ class Server(asyncore.dispatcher):
         self.users = {} # 用于比较是否有昵称冲突
         # hall = Hall(self)
         self.hall = Hall(self)   # server 启动便实例化 Hall ，作为 server 属性。同时 ，初始化hall，将 server 存为 hall 属性
+        self.python = Python(self)
+        self.write = Write(self)
+        self.pm = Pm(self)
         print self.hall, 'hall at server'
+        # <__main__.Hall instance at 0x1011105a8> hall at server
 
     def handle_accept(self):
         conn,addr = self.accept()
         print conn, addr
+        # <socket._socketobject object at 0x10072c520> ('127.0.0.1', 53362)
         ChatSession(self, conn)
-        # <socket._socketobject object at 0x10052d360> ('127.0.0.1', 62127)
 
 class ChatSession(asynchat.async_chat, CommandHandler):
     
@@ -83,7 +97,7 @@ class ChatSession(asynchat.async_chat, CommandHandler):
         asynchat.async_chat.__init__(self, sock)
         self.set_terminator('\r\n')
         self.data = []
-        self.clint_name = ''
+        self.client_name = ''
         # print self
         # <__main__.ChatSession connected 127.0.0.1:62127 at 0x10070c710>
         self.enter(server.hall)
@@ -114,8 +128,12 @@ class Hall(Room):
 
     def add(self, session):
         self.sessions.append(session) # 将新的会话添加到 sessions 列表。sessions属性继承自 Room
-        session.send("welcome to IRC server!\r\n")
-        session.send("Please log in use:\r\n/login name\r\n")
+        # print session.client_name
+        if session.client_name == '':
+            session.send("welcome to IRC server!\r\n")
+            session.send("Please log in use:\r\n/login name\r\n")
+        else:
+            session.send("you now back to Hall\r\n")
     
     def do_login(self, session, line):
         name = line.strip()
@@ -125,19 +143,22 @@ class Hall(Room):
             session.send('The name "%s" is taken.\r\n' % name)
             session.send('Please try again.\r\n')
         else:
-            session.clint_name = name
-            self.server.users[session.clint_name] = session
+            session.client_name = name
+            self.server.users[session.client_name] = session
             # print self.server.users, 1
-            session.send('Welcome, %s\r\n' % session.clint_name)
-            session.send("\r\nChatRoom list:\r\npython\r\nwrite\r\npm\r\n\r\nPlease log in use:\r\n/ChatRoom_name\r\n")
+            session.send('Welcome, %s\r\n' % session.client_name)
+            session.send("""\r\nChatRoom list:\r\npython\r\nwrite\r\npm
+            \r\nPlease log in use:\r\n/ChatRoom_name\r\n""")
 
     def do_logout(self, session, line):
-        del self.server.users[session.clint_name]
+        del self.server.users[session.client_name]
         Room.remove(self, session)
         Room.do_logout(self, session, line)
 
-    def do_python(self):
-        pass
+    def do_python(self, session,line):
+        print self.server.python
+        # <__main__.Python instance at 0x1011105f0>
+        session.enter(self.server.python)
 
     def do_write(self):
         pass
@@ -146,7 +167,38 @@ class Hall(Room):
         pass
 
 class Python(Room):
-    pass
+
+    def add(self, session):
+        self.sessions.append(session) # 将新的会话添加到 sessions 列表。sessions属性继承自 Room
+        session.send("welcome to ChatRoom 'Python'!\r\n")
+        # Room.broadcast(self, session.client_name + ' has entered the room.') # wrong
+        # self.broadcast(session.client_name + ' has entered the room.') # wrong
+        # print self.broadcast
+        # <bound method Python.broadcast of <__main__.Python instance at 0x1007105f0>>
+        # print self
+        # <__main__.Python instance at 0x1007105f0>
+        self.broadcast(session, session.client_name + ' has entered the room.')
+
+    def do_online(self, session, line):
+        users = self.sessions
+        # print users
+        # [<__main__.ChatSession connected 127.0.0.1:53362 at 0x101110830>]
+        if len(users) == 1: session.send('Nobody.\r\n')
+        for i in users:
+            if i != session:
+                session.send(i.client_name + '\r\n')
+
+    def do_back(self, session, line):
+        session.enter(self.server.hall)
+        # print Room
+        # __main__.Room
+        # print self
+        # <__main__.Python instance at 0x1011105f0>
+        # Room.remove(self, session)
+        self.remove(session)
+
+    def do_broadcast(self, session, line):
+        self.broadcast(session, session.client_name + ': ' + line)
 
 class Write(Room):
     pass
@@ -156,6 +208,8 @@ class Pm(Room):
 
 if __name__ == '__main__':
     s = Server(port, host, hall_name)
+    # print s
+    # <__main__.Server listening 127.0.0.1:5000 at 0x100732dd0>
     try:
         asyncore.loop()
     except KeyboardInterrupt:
